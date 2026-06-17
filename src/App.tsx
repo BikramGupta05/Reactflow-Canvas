54c133c75b802b8c93ab3682f368a0e935bcc8e7
BikramGupta05 <bikra2345@gmail.com>
2026-06-17 15:45:03 +0530

    Setup react vite ts tailwind shadcn foundation

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <main className="flex h-screen items-center justify-center">
+      <Button>Open Builder</Button>
+    </main>
+  );
+}
+
+export default App;
